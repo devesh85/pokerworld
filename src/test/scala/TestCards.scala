3cb5048f327d3526eb63731370d8ab1480f8790f
devesh <deveshk@gmail.com>
2017-09-10 23:59:53 -0400

    a working test case

--- src/test/scala/TestCards.scala
+++ src/test/scala/TestCards.scala
@@ -1,13 +1,16 @@
 import Cards.{Ace, Card, Clubs}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FlatSpec, Matchers}
 
 /**
   * Created by devesh on 9/10/17.
   */
-object TestCards extends App{
-  println("we will test here")
-
-  val newCard:Card = (Clubs, Ace)
-
+@RunWith(classOf[JUnitRunner])
+class TestCards  extends FlatSpec with Matchers{
+  "set intersection" should "be commutative" in{
+    1 should be (1)
+  }
 
 
 }
