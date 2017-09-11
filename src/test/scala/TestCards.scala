24ba45b3a72d7425ffd978ff3ee3f99143de15de
devesh <deveshk@gmail.com>
2017-09-10 23:43:30 -0400

    first cut

---
+++ src/test/scala/TestCards.scala
@@ -0,0 +1,13 @@
+import Cards.{Ace, Card, Clubs}
+
+/**
+  * Created by devesh on 9/10/17.
+  */
+object TestCards extends App{
+  println("we will test here")
+
+  val newCard:Card = (Clubs, Ace)
+
+
+
+}
